681ffcad4be0609231b8c52b5a951916d906109c
a8v0r6 <a8v0r6@protonmail.com>
2026-04-01 14:23:03 +0530

    conditional chaining

---
+++ conditional_chain.py
@@ -0,0 +1,51 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableBranch, RunnableLambda
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+class Feedback(BaseModel):
+
+    sentiment: Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
+
+parser = StrOutputParser()
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
+)
+
+prompt2 = PromptTemplate(
+    template='Write an appropriate response to this positive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriate response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+classifier_chain = prompt1 | model | parser2
+
+branch_chain = RunnableBranch(
+    (lambda x:x.sentiment == 'positive', prompt2 | model | parser),
+    (lambda x:x.sentiment == 'negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: "could not determine sentiment")
+)
+
+chain = classifier_chain | branch_chain
+
+res = chain.invoke({'feedback' : 'I give it 1 star'})
+
+print(res)
+
+chain.get_graph().print_ascii()
